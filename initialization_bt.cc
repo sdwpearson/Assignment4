539b832215ce8b249742d6de53f22604f84c0bf9
sdwpearson <sdwpearson@gmail.com>
2019-02-10 16:14:18 -0500

    [PHY1610] Add a boost check to assert that the number of ants is correct in the array

--- initialization_bt.cc
+++ initialization_bt.cc
@@ -21,11 +21,12 @@ BOOST_AUTO_TEST_CASE(initialize_uniform_test)
 {
 	int num_tests = 10;			// total number of tests to run
 	int max_total = 100;		// maximum total number of ants for the random generator
-	int max_length = 8;		// maximum length of the table dimensions for the random generator
+	int max_length = 8;			// maximum length of the table dimensions for the random generator
+	int total_cnt = 0;			// counts all of the ants to make sure it's the same as the specified total
 
 	for (int test = 0; test < num_tests; test++)
 	{
-	    static std::mt19937 engine(test+1);				// Create two random numbers to test the array
+	    static std::mt19937 engine(test+1);				// Create two random numbers to test the array initialization
 	    std::uniform_int_distribution<int> uniformint_length(2, max_length);
 	    std::uniform_int_distribution<int> uniformint_total(1, max_total);
 
@@ -33,11 +34,24 @@ BOOST_AUTO_TEST_CASE(initialize_uniform_test)
 		int total = uniformint_total(engine);			// total number of ants on the table
 		rarray<int,2> number_of_ants(length,length);
 
+		total_cnt = 0;
+
 		initialize_uniform(number_of_ants, total);
+
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < length; j++)
+			{	
+				total_cnt = total_cnt + number_of_ants[i][j]; // count the total number of ants
+			}
+		}
 
 		std::cout << "Test number: " << test << std::endl;
 		std::cout << "Length: " << length << " Total: " << total << std::endl;
-		std::cout << number_of_ants << std::endl;		
+		std::cout << number_of_ants << std::endl;	
+
+		// Make sure the total number of ants initialized in the array is correct
+		BOOST_CHECK(total==total_cnt);	
 	}
 }
 
